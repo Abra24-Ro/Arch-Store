39b37ff23936813f95a70d78142a1fcdbac0ae36
robraro <robinsonabra24@gmail.com>
2026-05-11 23:05:49 -0500

    fin de la seccion 16

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,20 +1,56 @@
 import { prisma } from "../lib/prisma";
-import { initialData } from "./seed";
+import { initialData, ValidTypes } from "./seed";
 
-async function seedDatabase() {
-  
-     await Promise.all([
-        prisma.productImage.deleteMany(),
-        prisma.product.deleteMany(),
-        prisma.category.deleteMany(),
-     ])
-    
- 
-   
+async function main() {
+  //** 1.Borrar registros previos
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
 
-;}
+  //** 2. Insertar datos iniciales
+
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((category) => ({
+    name: category,
+  }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  //** 2.1 Insertar categorías
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  const getCategoryId = (type: ValidTypes): string => {
+    const id = categoriesMap[type];
+    if (!id) throw new Error(`Categoría "${type}" no encontrada en la DB`);
+    return id;
+  };
+
+  //** 3. Insertar productos
+
+  for (const { images, type, ...productData } of products) {
+    await prisma.product.create({
+      data: {
+        ...productData,
+        categoryId: getCategoryId(type),
+        productImages: {
+          createMany: {
+            data: images.map((url) => ({ url })),
+          },
+        },
+      },
+    });
+  }
+}
 
 (() => {
   if (process.env.NODE_ENV === "production") return;
-  seedDatabase();
+  main();
 })();
